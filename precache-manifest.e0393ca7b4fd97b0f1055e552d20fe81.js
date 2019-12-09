9e01372ef25227e382d6023c5c46458dbd9cb213
Mansi <mansikamra00@gmail.com>
2019-12-09 14:40:03 +0530

    Updates

---
+++ precache-manifest.e0393ca7b4fd97b0f1055e552d20fe81.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f4859ff1d39af182eb8c31788dbd780",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "9a7343f05bd0ab03d4ba",
+    "url": "/react-gh-pages/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f2a49d045aa3a0cac43a",
+    "url": "/react-gh-pages/static/js/2.2b1f4b48.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-gh-pages/static/js/2.2b1f4b48.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9a7343f05bd0ab03d4ba",
+    "url": "/react-gh-pages/static/js/main.2bfd911b.chunk.js"
+  },
+  {
+    "revision": "3224d2eddfda86cb41db",
+    "url": "/react-gh-pages/static/js/runtime-main.38557597.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-gh-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
